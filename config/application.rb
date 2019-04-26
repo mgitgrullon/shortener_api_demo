56b83c7c3e36fc0c5cc4e7f5b122dd08972f80a1
Mario Grullon <mitgrullon@hotmail.com>
2019-04-26 12:37:33 -0400

    Update Rack Cors config

--- config/application.rb
+++ config/application.rb
@@ -24,13 +24,6 @@ module UrlShortener
     config.active_job.queue_adapter = :delayed_job
     config.middleware.use Rack::Attack
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
